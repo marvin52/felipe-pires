046cba82d4014ba5a337cc20615495d6abab6ed7
msmedeiros <msmedeiros@sp.r7.com>
2016-09-27 19:34:11 -0300

    update layout

--- source/js/main.js
+++ source/js/main.js
@@ -7,7 +7,7 @@ var youtubeApi = {
     this.updateVideos();
     if('serviceWorker' in navigator) {
       navigator.serviceWorker
-               .register('felipe-pires/dist/js/service-worker.js')
+               .register('dist/js/service-worker.js')
                .then(function() { console.log('Service Worker Registered'); });
     }
   },
